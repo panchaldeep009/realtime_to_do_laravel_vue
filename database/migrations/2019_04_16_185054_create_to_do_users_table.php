b2c13b82a725e3fce5f2177569d51684a36620ca
Deep Panchal <panchaldeep009@gmail.com>
2019-04-16 15:27:47 -0400

    to task users table change, add CSRF column

---
+++ database/migrations/2019_04_16_185054_create_to_do_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToDoUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_do_users', function (Blueprint $table) {
+            $table->increments('to_do_user_id');
+            $table->string('to_do_user_name');
+            $table->string('to_do_user_csrf', 100)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_do_users');
+    }
+}

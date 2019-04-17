d42e2726241ebfc6b53ba7050e7c803217d4a37f
Deep Panchal <panchaldeep009@gmail.com>
2019-04-17 15:20:55 -0400

    Add Center container

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,17 @@
 <template>
-    <p>Hello World</p>
+    <section class="center-container">
+        <div class="center">
+            <p>Hello World</p>
+        </div>
+    </section>
 </template>
+<script>
+export default {
+    mounted() {
+        
+    },
+}
+</script>
+<style lang="scss" scoped>
+    @import url("../sass/component/App.scss");
+</style>

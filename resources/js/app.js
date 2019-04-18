0cc1e171d09f349940eec740fcf402cc040b4bd9
Deep Panchal <panchaldeep009@gmail.com>
2019-04-17 22:49:41 -0400

    Fetch Tesks added,  Fetch replace with Axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,44 +1,48 @@
 import Vue from "vue";
 import App from "./App.vue";
+import axios from 'axios';
 
 new Vue({
     el: "#app",
     data: {
-        user: true,
+        user: false,
         users: [],
+        tasks: [],
         UserRefresh: null
     },
     methods: {
-        fetchJSON(url, success, error, perms){
-            fetch(url, {
-                ...perms,
-                headers: {
-                    "X-CSRF": window.Laravel.csrfToken
-                }
-            })
-            .then(data => data.json())
-            .then(response => {
+        fetchUser(){
+
+            axios.get('/api/users',
+                { headers: { "X-CSRF": window.Laravel.csrfToken } })
+            .then((response) => {
                 if(response.status == 200){
-                    success(response);
+                    this.users = response.data.users
+                    this.user = response.data.this_user
                 } else {
-                    error(response);
+                    this.errors.fetchUser = error;
                 }
             });
 
         },
-        fetchUser(){
-            this.fetchJSON("/api/users", 
-                ({this_user, users}) => {
-                    this.users = users
-                    this.user = this_user
-                }, 
-                ({error}) => this.errors.fetchUser = error);
+        fetchTasks(){
+            axios.get('/api/tasks',
+                { headers: { "X-CSRF": window.Laravel.csrfToken } })
+            .then((response) => {
+                if(response.status == 200){
+                    this.tasks = response.data.tasks
+                } else {
+                    this.errors.fetchTasks = error;
+                }
+            });
+
         },
     },
     mounted() {
         // V 1.0 Http Request Real-time
         this.UserRefresh = setInterval(() => {
             this.fetchUser();
+            this.fetchTasks();
         }, 1200);
 
         // V 2.0 Websocket Event

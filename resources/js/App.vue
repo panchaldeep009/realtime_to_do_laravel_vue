9716909dee143e1b377bcbbf1b634c863311a0a3
Deep Panchal <panchaldeep009@gmail.com>
2019-04-17 22:47:08 -0400

    Task Component Added

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,13 @@
 <template>
     <section class="center-container">
+        <h3>To Do Task List App</h3>
+        <hr>
         <div class="center">
             <RegisterUser v-if="!$root.user" />
-            <Users v-if="$root.user" :user="$root.user" :users="$root.users"/>
+            <div class="row">
+                <Users v-if="$root.user" :user="$root.user" :users="$root.users"/>
+                <Tasks v-if="$root.user" :tasks="$root.tasks"/>
+            </div>
         </div>
     </section>
 </template>
@@ -10,9 +15,10 @@
 // Components
 import RegisterUser from './components/RegisterUser';
 import Users from './components/Users';
+import Tasks from './components/Tasks';
 export default {
     name: 'App',
-    components: { RegisterUser, Users },
+    components: { RegisterUser, Users, Tasks },
 }
 </script>
 <style lang="scss" scoped>
